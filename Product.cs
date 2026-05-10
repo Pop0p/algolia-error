7fc15e9ad6e73935c7076a6d4a7bc40b1fb5e609
Dylan <dylan.dromard@altazion.com>
2026-05-10 18:41:43 +0200

    chore : parsing error

---
+++ Product.cs
@@ -0,0 +1,47 @@
+﻿namespace AlgoliaError;
+
+
+public record Product(
+    string ObjectID,
+    Guid Guid,
+    string Libelle,
+    string ImageBig,
+    string ImageSmall,
+    string Reference,
+    Attribute Marque,
+    bool EstDispoWeb,
+    bool EstPublie,
+    bool EnPromo,
+    bool IsEmbargo,
+    bool IsPreOrder,
+    DateOnly EmbargoDate,
+    decimal Prix,
+    decimal? PrixOriginal,
+    string PourcentagePromo,
+    double Note,
+    double? AgeEnMoisMin,
+    string Link,
+    double NotePourFacette,
+    decimal NombreVotes,
+    IReadOnlyDictionary<string, object> Attributs,
+    Statistique Statistiques,
+    Tag[] Tags,
+    string[]? Keywords,
+    string[]? Magasins
+);
+
+public readonly record struct Attribute(
+    string Code,
+    string Name
+);
+
+public readonly record struct Tag(
+    int Ordre,
+    string Html
+);
+
+public readonly record struct Statistique(
+    int Commandes,
+    int Visites,
+    int? Position
+);
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿// 7.39.0 => OK
+
+// 7.39.1 => KO
+// 7.40.0 => KO
+// 7.41.0 => KO
+// 7.42.0 => KO
+// Replace with your own Algolia credentials and index name.
+// Adding "hits" to the facets array at row 64 fixes the issue.
+
+using Algolia.Search.Clients;
+using Algolia.Search.Models.Search;
+using AlgoliaError;
+
+var client = new SearchClient("XXX", "XXX");
+const string index = "XXX";
+
+string[] allFacetKeys =
+[
+    "vitrines.name",
+    "licence.name",
+    "marque.name",
+    "prix",
+    "notePourFacette",
+    "agesEnMois",
+    "keywords",
+    "categories.lvl0.name",
+    "categories.lvl1.name"
+];
+
+
+// Let's say marque.name has been used as an active filter.
+var activeFacetKeys = allFacetKeys.Where(x => x == "marque.name").ToArray();
+var inactiveFacetKeys = allFacetKeys.Except(activeFacetKeys).ToList();
+
+var searches = new SearchMethodParams
+{
+    Requests =
+    [
+        new SearchQuery(new SearchForHits
+        {
+            IndexName = index,
+            Query = "Pokemon",
+            Page = 0,
+            HitsPerPage = 50,
+            Filters = "marque.name: \"Bandai\"",
+            AttributesToRetrieve =
+            [
+                "libelle", "reference", "estDispoWeb", "enPromo", "marque", "estPublie", "vitrines", "magasins",
+                "note", "nombreVotes", "prixOriginal", "imageSmall", "link", "pourcentagePromo", "prix", "tags"
+            ],
+            Facets = [..inactiveFacetKeys],
+            AttributesToHighlight = [],
+            GetRankingInfo = false,
+            ResponseFields = ["page", "nbHits", "nbPages", "hits", "facets", "facets_stats", "renderingContent"]
+        }),
+        new SearchQuery(new SearchForHits
+        {
+            IndexName = index,
+            Query = "Pokemon",
+            HitsPerPage = 0,
+            Facets = [..activeFacetKeys],
+            AttributesToHighlight = [],
+            GetRankingInfo = false,
+            ResponseFields = ["facets"], // <= If i add "hits", then it works. 
+        })
+    ],
+};
+
+try
+{
+    var results = await client.SearchAsync<Product>(searches);
+    Console.WriteLine("Success");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
